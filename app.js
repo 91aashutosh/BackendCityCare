1f5136673c2c8551ced1bb8e6ea4fe74286c2769
91aashutosh <aashu2348154@gmail.com>
2024-03-05 04:33:53 +0530

    initial commit pushed 4 APIs - register, login, OTP send and OTP verify

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+const connectDB = require("./database/connect");
+const loginSignupRoute = require("./routes/loginSignupRoute");
+connectDB();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+    "/",
+    loginSignupRoute,
+  );
+
+  app.listen(process.env.PORT ? process.env.PORT : 5000, () => {
+    console.log(`app listening on PORT ${process.env.PORT}`);
+  })
---
+++ routes/loginSignupRoute.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+var session = require("express-session");
+var cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
+var loginSignupController = require("../controller/loginSignupController");
+const auth = require("../middleware/auth");
+//session
+const oneDay = 1000 * 60 * 60 * 24;
+router.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
+router.use(cookieParser());
+
+router.post("/api-logout", function (req, res) {
+    req.session.destroy();
+    res.json("logout success");
+  });
+
+  router.post("/api_send_otp", loginSignupController.api_send_otp);
+  router.post("/api_verify_otp", loginSignupController.api_verify_otp);
+  router.post("/api_citizen_registration", loginSignupController.api_citizen_registration)
+  router.post("/api_citizen_login", loginSignupController.api_citizen_login);
+
+module.exports = router;
+
+
+  //2DULT62PHCFMYQBDE6AX1UGU
+
