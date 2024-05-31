dbb3e926ca1a1ff9385e343481ea671749292a59
91aashutosh <aashu2348154@gmail.com>
2024-05-31 19:23:15 +0530

    DB_URL changed to MONGO_URL

--- Backend/database/connect.js
+++ Backend/database/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // mongodb connection string
-        const con = await mongoose.connect(process.env.DB_URL, {
+        const con = await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
